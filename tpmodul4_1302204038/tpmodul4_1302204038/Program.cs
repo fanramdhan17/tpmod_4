6903815c3f435fecd8b0d7bfc14f7a1540cc4b78
luthfanrw17 <olioaglio55@gmail.com>
2022-03-18 17:53:49 +0700

    Menambahkan class HaloGeneric

--- tpmodul4_1302204038/tpmodul4_1302204038/Program.cs
+++ tpmodul4_1302204038/tpmodul4_1302204038/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302204038
+{
+    public class HaloGeneric<T>
+    {
+        public void SapaUser(T x)
+        {
+            Console.WriteLine("halo" + x);
+        }
+    }
+}
+
